5f86d6c77e603d07c46acbe43b053b837e0e8657
anastasialamberg <anastasia.lamberg@gmail.com>
2025-10-03 21:39:34 +0300

    Fixes

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDwBqNkdSLfrLxHEOOQXc_nWrbEuFAX0nI",
+  authDomain:"kirjahylly-619c7.firebaseapp.com",
+  projectId: "kirjahylly-619c7",
+  storageBucket: "kirjahylly-619c7.firebasestorage.app",
+  messagingSenderId: "937349323108",
+  appId: "G-8PT2ZS6RRW",
 };
 
 const app = initializeApp(firebaseConfig);
