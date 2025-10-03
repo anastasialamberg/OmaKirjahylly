ca1cfebd0a323a112bc13216fe2d8dbcbce9ded8
anastasialamberg <anastasia.lamberg@gmail.com>
2025-10-03 21:48:27 +0300

    Fixes

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,31 +1,37 @@
+import { useNavigate } from "react-router-dom";
 
 function Homepage() {
+  const navigate = useNavigate();
   return (
     <div className="min-h-screen ">
-    
-
-    
       <div className="grid place-items-center min-h-screen px-4">
         <div className="bg-gray-200 rounded-2xl shadow-xl p-10 max-w-lg w-full text-center">
           <h1 className="text-4xl font-extrabold text-gray-900 mb-6">
-            Tervetuloa kirjahyllyyn 
+            Tervetuloa kirjahyllyyn
           </h1>
           <p className="text-gray-600 mb-8">
-            Hallitse ja selaa kirjoja helposti. Aloita lisäämällä uusi kirja tai tutustu kokoelmaasi!
+            Hallitse ja selaa kirjoja helposti. Aloita lisäämällä uusi kirja tai
+            tutustu kokoelmaasi!
           </p>
           <div className="flex flex-col gap-4">
-            <button className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-5 rounded-xl shadow transition text-center" onClick={() => window.location.href = '/booksearch'}>
+            <button
+              className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-5 rounded-xl shadow transition text-center"
+              onClick={() => navigate("/booksearch")}
+            >
               Lisää uusi kirja
             </button>
-            <button className="bg-gray-200 hover:bg-gray-300 text-gray-800 py-3 px-5 rounded-xl shadow transition" onClick={() => window.location.href = '/booklist'}>
+
+            <button
+              className="bg-gray-200 hover:bg-gray-300 text-gray-800 py-3 px-5 rounded-xl shadow transition"
+              onClick={() => navigate("/booklist")}
+            >
               Selaa kirjoja
-              
             </button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Homepage;
